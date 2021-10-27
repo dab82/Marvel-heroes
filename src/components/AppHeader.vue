b78ab2005524c7871ee9439e3f34f7d323ee94e6
Dmitriy Bondarenko <dabwork1982@gmail.com>
2021-10-27 23:58:46 +0300

    add vue.js

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,27 @@
+<template>
+	<nav class="navbar sticky-top navbar-dark bg-dark">
+		<div class="container-fluid">
+			<a class="navbar-brand logo" href="/index.html">MARVEL</a>
+			<form class="d-flex">
+				<input
+					class="form-control me-2"
+					type="search"
+					placeholder="Поиск ..."
+					aria-label="Search"
+				/>
+				<button class="btn btn-outline-danger" type="submit">GO</button>
+			</form>
+		</div>
+	</nav>
+</template>
+
+<script>
+export default {
+	name: "AppHeader",
+	data() {
+		return {};
+	},
+};
+</script>
+
+<style scoped></style>
