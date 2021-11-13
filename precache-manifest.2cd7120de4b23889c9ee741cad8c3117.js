9565eb1225e0a10439622a791692dc0f44f3440f
Dmitriy Bondarenko <dabwork1982@gmail.com>
2021-11-13 22:50:20 +0200

    0

---
+++ precache-manifest.2cd7120de4b23889c9ee741cad8c3117.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0d7dac1872fa93a782252d5de72ccc0",
+    "url": "/Marvel-heroes/index.html"
+  },
+  {
+    "revision": "1b04bd23aef1cb9beecc",
+    "url": "/Marvel-heroes/js/app.30a056d2.js"
+  },
+  {
+    "revision": "a3873639a6223bac2cca",
+    "url": "/Marvel-heroes/js/chunk-vendors.b53b4d9f.js"
+  },
+  {
+    "revision": "7d847ac1b23eec3526716581798a8368",
+    "url": "/Marvel-heroes/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Marvel-heroes/robots.txt"
+  }
+]);
